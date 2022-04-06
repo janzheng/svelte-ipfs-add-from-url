a78fdb7436bee55b2cdce6709ef76c825c7ecb9c
janzheng <janeazy@gmail.com>
2022-04-06 20:33:47 +1000

    lol

---
+++ src/config.js
@@ -0,0 +1,15 @@
+
+// local
+export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const rpcaddress = null
+
+// mumbai
+// export const nftmarketaddress = "0x4cA758E41120E0C579eE3589EBad54fBF1CB1839"
+// export const nftaddress = "0x07Ef265D7767733D97F352DF69596A4f165a214b"
+// export const rpcaddress = "https://rpc-mumbai.maticvigil.com/v1/ae56aec4991688b675fc5833fca07ff22ade6aa4"
+
+// matic
+// export const nftmarketaddress = "0x244b3984bfCB62D65E2B66a42a7889D04917174e"
+// export const nftaddress = "0xE41F939bDf78868215B505a4E01cB5e91C4bcdb8"
+// export const rpcaddress = "https://rpc-mainnet.maticvigil.com/v1/ae56aec4991688b675fc5833fca07ff22ade6aa4"
---
+++ src/lib/layouts/Header.svelte
@@ -0,0 +1,39 @@
+
+<script>
+	// import { page } from '$app/stores';
+	// import logo from '../../../static/labdao-full-2x.png'; // static necessary for ipfs
+
+	import Desktop from '$lib/components/shared/MenuDesktop.svelte';
+	import Mobile from '$lib/components/shared/MenuMobile.svelte';
+  
+</script>
+
+
+<!-- <div class="_content mt-4">
+  <nav class="border-b p-6">
+    <p class="text-4xl font-bold">Metaverse Marketplace</p>
+    <div class="flex mt-4">
+      <a class="mr-6 text-pink-500" sveltekit:prefetch href="/">
+        Home
+      </a>
+      <a class="mr-6 text-pink-500" sveltekit:prefetch href="/create-item">
+        Sell Digital Asset
+      </a>
+      <a class="mr-6 text-pink-500" sveltekit:prefetch href="/my-assets">
+        My Digital Assets
+      </a>
+      <a class="mr-6 text-pink-500" sveltekit:prefetch href="/creator-dashboard">
+        Creator Dashboard
+      </a>
+    </div>
+  </nav>
+</div> -->
+
+<!-- <Mobile {logo} {page} /> -->
+
+<!-- 
+<style lang="postcss">
+  /* .popover__menu {
+    @apply bg-white shadow-md rounded-md p-2 w-32;
+  } */
+</style> -->
